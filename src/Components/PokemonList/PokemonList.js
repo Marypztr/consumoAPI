1af1cd6b0766ab987fe60f02b6d79b74a235bb07
Marypztr <nutrimary.77@gmail.com>
2019-10-22 21:46:40 -0500

    imagenes aun no se pueden pintar

--- src/Components/PokemonList/PokemonList.js
+++ src/Components/PokemonList/PokemonList.js
@@ -9,20 +9,34 @@ export default class PokemonList extends Component {
   };
 
   componentDidMount(){
-    axios.get(`https://pokeapi.co/api/v2/pokemon/?offset=0&limit=964`)
+    
+    axios.get(`https://pokeapi.co/api/v2/pokemon/?offset=0&limit=20`)
     .then(res =>{
-        console.log(res.data.results)
-
       this.setState({ pokemons: res.data.results })
-    })
-
+    })  
     //?offset=20&limit=20
   }
+
+  test(pokeName) {
+    axios.get(`https://pokeapi.co/api/v2/pokemon/` + pokeName)
+    .then(res => {
+      console.log(res.data.sprites.front_default)
+      return res.data.sprites.front_default
+    })
+  }
+
   render() {
   return(
-      <div>
+      <div className={styles.container}>
         <form>
-            {this.state.pokemons.map(pokemon => <div  key={pokemon.id}> the pokemon name is: {pokemon.name}</div>)}
+            {
+              this.state.pokemons.map(pokemon =>
+                <React.Fragment>            
+                  <div key={pokemon.id}> the pokemon name is: {pokemon.name}</div>
+                  <div>{this.data.sprites.front_default}</div>
+                </React.Fragment>
+              )
+          }
         </form>  
       </div>
   )  
